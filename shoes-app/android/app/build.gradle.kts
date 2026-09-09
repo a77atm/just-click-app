4f56c1de8ac99fbbed6c98c0d8f653d97cf8383e
a77atm <anamido76@gmail.com>
2026-09-09 15:57:42 +0300

    Add Kotlin Android plugin to build.gradle.kts

--- shoes-app/android/app/build.gradle.kts
+++ shoes-app/android/app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Reads android/app/google-services.json at build time.
+    id("com.google.gms.google-services")
 }
 
 android {
